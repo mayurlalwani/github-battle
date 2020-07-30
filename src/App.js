fd4bacb9e44787421e0193b2d3441d79a7134fa2
mayurlalwani <mayur_lalwani4@ymail.com>
2020-07-30 20:52:00 +0530

    Added search users functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Layout/Navbar'
 import Users from './Components/Users/Users'
+import Search from './Components/Users/Search'
+
 import axios from 'axios'
 import './index.css'
 
@@ -11,20 +13,36 @@ class App extends Component{
     loading: false
   }
 
-  async componentDidMount(){
-    this.setState({loading: true})
-    const res = await axios.get("https://api.github.com/users");
+  // async componentDidMount(){
+  //   this.setState({loading: true})
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  //   this.setState({
+  //     users: res.data,
+  //     loading: false
+  //   })
+  // }
+
+  searchUsers = async text => {
+    this.setState({loading:true})
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({
-      users: res.data,
+      users: res.data.items,
       loading: false
     })
   }
+
+  clearUsers = () => {
+    this.setState({ users:[], loading: false})
+  }
+
   render(){  
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar title="Github Finder" />
         <div className="container">
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true : false} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
