9def98951afcf7e6dead4dadf5c2b103f2c89fdd
mayurlalwani <mayur_lalwani4@ymail.com>
2020-07-30 08:53:54 +0530

    Converted to functional component

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Navbar extends Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired
-    }
-    state = {  }
-    render() { 
-        return ( 
-            <nav className="navbar bg-primary">
-                <h1>{this.props.title}</h1>
-            </nav>
-        )
-    }
+function Navbar(props) {
+
+    return ( 
+        <nav className="navbar bg-primary">
+            <h1>{props.title}</h1>
+        </nav>
+    )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired
 }
  
 export default Navbar;
