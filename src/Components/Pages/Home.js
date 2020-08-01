8060f9843dcf26d3927c66dc9f99e52055d924b3
mayurlalwani <mayur_lalwani4@ymail.com>
2020-08-01 12:07:52 +0530

    Added Home component

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from '../Users/Search'
+import Users from '../Users/Users'
+
+function Home(){
+    return (
+    <>
+        <Search />
+        <Users />
+    </>
+    )
+}
+
+export default Home
