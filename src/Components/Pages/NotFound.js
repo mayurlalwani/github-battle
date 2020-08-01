1910573a19489e0138b7da9a503fd6096cd74585
mayurlalwani <mayur_lalwani4@ymail.com>
2020-08-01 12:08:07 +0530

    Added Not found component if url is invalid

---
+++ src/Components/Pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NotFound(){
+    return (
+        <div>
+            <h1>
+                Page Not Found
+            </h1>
+            <p className="lead">The page does not exists....</p>
+        </div>
+    )
+}
+
+export default NotFound
